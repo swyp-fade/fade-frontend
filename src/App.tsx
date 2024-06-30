d7d8b2f694aa2c47009865a27f2da7cc46f0d223
sangpok <48979587+sangpok@users.noreply.github.com>
2024-06-30 20:40:59 +0900

    🚧 Mocking 확인을 위한 임시 로직 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { useEffect } from 'react';
+
 export default function App() {
+  useEffect(() => {
+    fetch('https://example.com/user')
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+      });
+  }, []);
+
   return <main>hihi</main>;
 }
