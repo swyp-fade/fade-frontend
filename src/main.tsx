8cde4c678662123992484b67823173d77e09328f
sangpok <48979587+sangpok@users.noreply.github.com>
2024-06-30 20:38:30 +0900

    🔧 NODE_DEV 환경일 시 Mocking Logic 동작

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+export async function enableMocking() {
+  if (import.meta.env.PROD) {
+    return;
+  }
+
+  const { worker } = await import('./__mock__/instance.ts');
+
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+});
