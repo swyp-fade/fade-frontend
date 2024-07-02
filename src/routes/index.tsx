943e26ceea6811235701f7db924af35f211b0760
sangpok <48979587+sangpok@users.noreply.github.com>
2024-07-02 17:42:34 +0900

    ⚡️ NotFoundPage Lazy Import

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import RootLayout from '@/layouts/RootLayout';
 import { queryClient } from '@Libs/queryclient';
-import NotFoundPage from '@Pages/NotFoundPage';
 import RootPage, { loader as rootLoader } from '@Pages/Root/page';
+import { lazy } from 'react';
 import { Route, createRoutesFromElements } from 'react-router-dom';
+
+const NotFoundPage = lazy(() => import('@Pages/NotFoundPage'));
 
 export const routesFromElements = createRoutesFromElements(
   <Route path="/" element={<RootLayout />}>
     <Route index element={<RootPage />} loader={() => rootLoader({ queryClient })} />
-    <Route path="*" element={<NotFoundPage />} />
+    <Route path="*" Component={NotFoundPage} />
   </Route>
 );
