c5d89fcd84f499ff144b5cde5ecb12b8d9bdb8a2
sangpok <48979587+sangpok@users.noreply.github.com>
2024-07-05 23:06:09 +0900

    📸 기존 작업 병합

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -11,3 +11,47 @@ export function getPayloadFromJWT(jwt: string) {
     iat: Date;
   };
 }
+
+export type RequiredWith<T, K extends keyof T> = T & {
+  [P in K]-?: T[P];
+};
+
+export const isAxiosErrorWithCustomCode = (error: unknown): error is RequiredWith<AxiosError<ServiceErrorResponse>, 'response'> => {
+  return (
+    typeof error === 'object' &&
+    error !== null &&
+    isAxiosError<ServiceErrorResponse>(error) &&
+    !!error.response &&
+    !!error.response.data.errorCode &&
+    error.response.data.errorCode in SERVICE_ERROR_MESSAGE
+  );
+};
+
+export function createSuccessLoaderResponse<T>(payload: T): LoaderResponse<T> {
+  return { status: LoaderResponseStatus.SUCCESS, payload };
+}
+
+export function createErrorLoaderResponse<T = never>(payload: ServiceErrorResponse): LoaderResponse<T> {
+  return { status: LoaderResponseStatus.ERROR, payload };
+}
+
+export type TryCatcherResult<T> = [T, null] | [null, ServiceErrorCode];
+
+export const tryCatcher = async <T, _>(tryer: () => T | Promise<T>): Promise<TryCatcherResult<T>> => {
+  try {
+    const result = await tryer();
+    return [result, null];
+  } catch (error) {
+    if (isAxiosErrorWithCustomCode(error)) {
+      const { errorCode } = error.response.data;
+
+      return [null, errorCode];
+    }
+
+    return [null, 'unknown_error' as ServiceErrorCode];
+  }
+};
+
+export function clearSearchParams() {
+  history.pushState(null, '', window.location.pathname);
+}
