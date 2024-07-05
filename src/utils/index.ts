3be5338191c6cf4a3b2200adcbabcd571df7d26a
sangpok <48979587+sangpok@users.noreply.github.com>
2024-07-05 15:42:27 +0900

    ✨ Shadcn-ui의 util helper function 추가

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,6 @@
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
 export function getPayloadFromJWT(jwt: string) {
   return JSON.parse(atob(jwt.split('.')[1]).replaceAll('\\', '')) as {
     id: string;
@@ -6,4 +9,8 @@ export function getPayloadFromJWT(jwt: string) {
     exp: Date;
     iat: Date;
   };
+}
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
