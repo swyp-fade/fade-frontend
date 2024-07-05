5862f51578d0fdc8a96f337d7f6189e379a1a3eb
sangpok <48979587+sangpok@users.noreply.github.com>
2024-07-05 22:38:50 +0900

    ✨ 오류 관련 Helper 함수 및 Loader 반환 함수 및 tryCatcher 작성

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,7 @@
+import { SERVICE_ERROR_MESSAGE, ServiceErrorCode, ServiceErrorResponse } from '@Types/serviceError';
+import { LoaderResponse, LoaderResponseStatus } from '@Types/loaderResponse';
+import { AxiosError, isAxiosError } from 'axios';
+
 export function getPayloadFromJWT(jwt: string) {
   return JSON.parse(atob(jwt.split('.')[1]).replaceAll('\\', '')) as {
     id: string;
@@ -6,4 +10,48 @@ export function getPayloadFromJWT(jwt: string) {
     exp: Date;
     iat: Date;
   };
+}
+
+export type RequiredWith<T, K extends keyof T> = T & {
+  [P in K]-?: T[P];
+};
+
+export const isAxiosErrorWithCustomCode = (error: unknown): error is RequiredWith<AxiosError<ServiceErrorResponse>, 'response'> => {
+  return (
+    typeof error === 'object' &&
+    error !== null &&
+    isAxiosError<ServiceErrorResponse>(error) &&
+    !!error.response &&
+    !!error.response.data.errorCode &&
+    error.response.data.errorCode in SERVICE_ERROR_MESSAGE
+  );
+};
+
+export function createSuccessLoaderResponse<T>(payload: T): LoaderResponse<T> {
+  return { status: LoaderResponseStatus.SUCCESS, payload };
+}
+
+export function createErrorLoaderResponse<T = never>(payload: ServiceErrorResponse): LoaderResponse<T> {
+  return { status: LoaderResponseStatus.ERROR, payload };
+}
+
+export type TryCatcherResult<T> = [T, null] | [null, ServiceErrorCode];
+
+export const tryCatcher = async <T, _>(tryer: () => T | Promise<T>): Promise<TryCatcherResult<T>> => {
+  try {
+    const result = await tryer();
+    return [result, null];
+  } catch (error) {
+    if (isAxiosErrorWithCustomCode(error)) {
+      const { errorCode } = error.response.data;
+
+      return [null, errorCode];
+    }
+
+    return [null, 'unknown_error' as ServiceErrorCode];
+  }
+};
+
+export function clearSearchParams() {
+  history.pushState(null, '', window.location.pathname);
 }
