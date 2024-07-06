ae3505b18ec44235ba1139aac33eea79ccec78b6
sangpok <48979587+sangpok@users.noreply.github.com>
2024-07-06 23:13:57 +0900

    ♻️ Header Store 정보 반영한 Header 컴포넌트 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
-import { MdInfoOutline, MdOutlineNotificationsNone } from 'react-icons/md';
+import { useHeaderStore } from '@Stores/header';
 
 export function Header() {
+  const leftSlot = useHeaderStore((state) => state.leftSlot);
+  const rightSlot = useHeaderStore((state) => state.rightSlot);
+  const title = useHeaderStore((state) => state.title);
+
   return (
     <header className="relative px-5 py-4">
-      <MdInfoOutline className="absolute left-5 top-1/2 size-6 -translate-y-1/2" />
-      <p className="text-center text-2xl font-semibold">FA:P 투표</p>
-      <MdOutlineNotificationsNone className="absolute right-5 top-1/2 size-6 -translate-y-1/2" />
+      {leftSlot && <div className="absolute left-5 top-1/2 -translate-y-1/2">{leftSlot()}</div>}
+      {title && <p className="text-center text-2xl font-semibold">{title}</p>}
+      {rightSlot && <div className="absolute right-5 top-1/2 -translate-y-1/2">{rightSlot()}</div>}
     </header>
   );
 }
