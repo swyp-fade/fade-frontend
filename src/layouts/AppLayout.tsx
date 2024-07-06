3957b4716e45fc24c05f0f3e5bf9869bf09cd6a5
sangpok <48979587+sangpok@users.noreply.github.com>
2024-07-06 20:55:59 +0900

    ♻️ Header 및 NavBar 컴포넌트로 분리

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,13 +1,15 @@
+import { Header } from '@Components/Header';
+import { NavBar } from '@Components/NavBar';
 import { Outlet } from 'react-router-dom';
 
 export default function AppLayout() {
   return (
     <div className="flex h-full flex-col">
-      <header className="border border-red-300">헤더</header>
+      <Header />
       <main className="scroll min-h-1 flex-1 overflow-y-scroll border border-blue-300 p-5">
         <Outlet />
       </main>
-      <footer className="border border-green-500">풋터</footer>
+      <NavBar />
     </div>
   );
 }
