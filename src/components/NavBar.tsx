99578ff44b7cbea062d3fc61fefa9447249a3dd5
sangpok <48979587+sangpok@users.noreply.github.com>
2024-07-06 21:37:32 +0900

    ♻️ NavBar 메뉴 추가 확장성 고려

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,36 +1,61 @@
-import { MdOutlineGridOn, MdHowToVote, MdAdd, MdAccountBox, MdPerson } from 'react-icons/md';
-import { Link } from 'react-router-dom';
+import { cn } from '@Utils/index';
+import { useCallback } from 'react';
+import { IconType } from 'react-icons/lib';
+import { MdAccountBox, MdAdd, MdHowToVote, MdOutlineGridOn, MdPerson } from 'react-icons/md';
+import { NavLink } from 'react-router-dom';
+
+interface NavItem {
+  link: string;
+  IconComponent: IconType;
+}
+
+const navList: NavItem[] = [
+  {
+    link: '/archive',
+    IconComponent: MdOutlineGridOn,
+  },
+  {
+    link: '/vote-fap',
+    IconComponent: MdHowToVote,
+  },
+  {
+    link: '/upload',
+    IconComponent: MdAdd,
+  },
+  {
+    link: '/feed',
+    IconComponent: MdAccountBox,
+  },
+  {
+    link: '/mypage',
+    IconComponent: MdPerson,
+  },
+];
 
 export function NavBar() {
+  const memoizedCreateNavItem = useCallback((navItem: NavItem) => <NavItem key={navItem.link} {...navItem} />, []);
+
   return (
     <nav>
-      <ul className="flex flex-row">
-        <li className="flex-1">
-          <Link to="/archive" className="block h-full w-full py-5 transition-transform hover:rotate-3 hover:scale-125">
-            <MdOutlineGridOn className="mx-auto size-6" />
-          </Link>
-        </li>
-        <li className="flex-1">
-          <Link to="/archive" className="block h-full w-full py-5 transition-transform hover:rotate-3 hover:scale-125">
-            <MdHowToVote className="mx-auto size-6" />
-          </Link>
-        </li>
-        <li className="flex-1">
-          <Link to="/archive" className="block h-full w-full py-5 transition-transform hover:rotate-3 hover:scale-125">
-            <MdAdd className="mx-auto size-6" />
-          </Link>
-        </li>
-        <li className="flex-1">
-          <Link to="/archive" className="block h-full w-full py-5 transition-transform hover:rotate-3 hover:scale-125">
-            <MdAccountBox className="mx-auto size-6" />
-          </Link>
-        </li>
-        <li className="flex-1">
-          <Link to="/archive" className="block h-full w-full py-5 transition-transform hover:rotate-3 hover:scale-125">
-            <MdPerson className="mx-auto size-6" />
-          </Link>
-        </li>
-      </ul>
+      <ul className="flex flex-row">{navList.map(memoizedCreateNavItem)}</ul>
     </nav>
+  );
+}
+
+type NavItemProps = { link: string; IconComponent: IconType };
+
+function NavItem({ link, IconComponent }: NavItemProps) {
+  return (
+    <li className="flex-1">
+      <NavLink
+        to={link}
+        className={({ isActive }) =>
+          cn('block h-full w-full py-5 transition-transform hover:rotate-3 hover:scale-125 active:scale-95', {
+            ['text-purple-700']: isActive,
+          })
+        }>
+        <IconComponent className="mx-auto size-6" />
+      </NavLink>
+    </li>
   );
 }
