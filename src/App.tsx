428a19c90806ae35f6c9fd6a099529378e703a96
sangpok <48979587+sangpok@users.noreply.github.com>
2024-07-08 23:44:24 +0900

    ✨ Framer Motion 기본 모션 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,23 @@ import { queryClient } from '@Libs/queryclient';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { routesFromElements } from './routes';
+import { MotionConfig, Transition } from 'framer-motion';
 
+/** Router */
 const browserRouter = createBrowserRouter(routesFromElements);
+
+/** Motion Config */
+const transitionConfig: Transition = {
+  ease: [0.16, 1, 0.3, 1],
+  duration: 0.5,
+};
 
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={browserRouter} />
-    </QueryClientProvider>
+    <MotionConfig transition={transitionConfig}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={browserRouter} />
+      </QueryClientProvider>
+    </MotionConfig>
   );
 }
