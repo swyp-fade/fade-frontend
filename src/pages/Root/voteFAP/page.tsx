d1d2dbfd5a6531b5892b06148b3ab2f828fb3409
sangpok <48979587+sangpok@users.noreply.github.com>
2024-07-09 21:11:45 +0900

    🚧 Toast 동작 테스트를 위한 임시 코드

--- src/pages/Root/voteFAP/page.tsx
+++ src/pages/Root/voteFAP/page.tsx
@@ -1,3 +1,4 @@
+import { useToastActions } from '@Hooks/toast';
 import { useHeader } from '@Hooks/useHeader';
 import { useState } from 'react';
 import { MdInfoOutline, MdOutlineNotificationsNone } from 'react-icons/md';
@@ -9,9 +10,59 @@ export default function Page() {
     rightSlot: () => <RightSlotComponent />,
   });
 
+  const { showToast, closeToast } = useToastActions();
+
   return (
     <>
       투표 화면
+      <button
+        onClick={() => {
+          const toastId1 = showToast({
+            title: '사진 업로드 완료',
+            type: 'success',
+            actionSlot: () => (
+              <button
+                onClick={() => {
+                  alert('먼갈 보여줌');
+                  closeToast(toastId1);
+                }}
+                className="text-purple-700">
+                보러가기
+              </button>
+            ),
+          });
+          const toastId2 = showToast({
+            title: '사진 업로드 실패',
+            type: 'error',
+            actionSlot: () => (
+              <button
+                onClick={() => {
+                  alert('먼갈 다시 시도함');
+                  closeToast(toastId2);
+                }}>
+                다시시도
+              </button>
+            ),
+          });
+          const toastId3 = showToast({
+            title: '신고되었습니다.',
+            type: 'basic',
+          });
+          const toastId4 = showToast({
+            title: '어서오세요, FADE_1234님!',
+            type: 'welcome',
+          });
+          const toastId5 = showToast({
+            title: '이미 존재하는 ID입니다.',
+            type: 'error',
+          });
+          const toastId6 = showToast({
+            title: '업로드할 사진을 추가해주세요!',
+            type: 'error',
+          });
+        }}>
+        토스트 테스트
+      </button>
       <ul className="flex flex-col gap-6">
         {[1, 2, 3, 4, 5].map((_, index) => (
           <li key={index} className="aspect-[3/4] w-full rounded-lg bg-gray-100" />
