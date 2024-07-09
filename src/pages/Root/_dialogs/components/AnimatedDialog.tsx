c4577c5c4da10926eff2745ec1158f80daba42a5
sangpok <48979587+sangpok@users.noreply.github.com>
2024-07-09 15:23:06 +0900

    ♻️ Modal 타입에 따라 Visible Animation 설정
    Full Screen Dialog, Bottom Sheet일 때 애니메이션 분기

--- src/pages/Root/_dialogs/components/AnimatedDialog.tsx
+++ src/pages/Root/_dialogs/components/AnimatedDialog.tsx
@@ -1,16 +1,17 @@
+import { cn } from '@Utils/index';
 import { Variants, motion } from 'framer-motion';
 import { PropsWithChildren } from 'react';
 
 const slideUpVariants: Variants = {
   hide: { y: '100%', scale: '85%', opacity: '80%', transformOrigin: 'bottom' },
   show: { y: 0, scale: '100%', opacity: '100%' },
-  exit: { y: '100%', scale: '85%', opacity: '80%', transition: { duration: 0.3 } },
+  exit: { y: '100%', scale: '85%', opacity: '80%', transition: { duration: 0.2 } },
 };
 
 const slideInFromRightVariants: Variants = {
   hide: { x: '100%', scale: '85%', opacity: '80%', transformOrigin: 'right' },
   show: { x: '0', scale: '100%', opacity: '100%' },
-  exit: { x: '100%', scale: '85%', opacity: '80%', transition: { duration: 0.3 } },
+  exit: { x: '100%', scale: '85%', opacity: '80%', transition: { duration: 0.2 } },
 };
 
 type AnimateType = 'slideUp' | 'slideInFromRight';
@@ -20,14 +21,27 @@ const variantsMap: Record<AnimateType, Variants> = {
   slideInFromRight: slideInFromRightVariants,
 };
 
-export function AnimatedDialog({ animateType = 'slideUp', children }: PropsWithChildren<{ animateType?: AnimateType }>) {
+type ModalType = 'fullScreenDialog' | 'bottomSheet';
+
+type AnimatedDialogProps = {
+  animateType?: AnimateType;
+  modalType?: ModalType;
+};
+
+export function AnimatedDialog({ animateType = 'slideUp', modalType = 'fullScreenDialog', children }: PropsWithChildren<AnimatedDialogProps>) {
+  const isFullScreenDialog = modalType === 'fullScreenDialog';
+  const isBottomSheet = modalType === 'bottomSheet';
+
   return (
     <motion.div
       initial="hide"
       animate="show"
       exit="exit"
       variants={variantsMap[animateType]}
-      className="pointer-events-auto absolute bottom-0 left-0 flex h-full w-full flex-col bg-white">
+      className={cn('pointer-events-auto absolute flex w-full flex-col bg-white', {
+        ['bottom-0 left-0 h-full']: isFullScreenDialog,
+        ['bottom-0 left-0 h-fit rounded-t-2xl']: isBottomSheet,
+      })}>
       {children}
     </motion.div>
   );
