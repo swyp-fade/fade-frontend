49924efcaca87eb1b4fcc16f6cef6cee68675d33
sangpok <48979587+sangpok@users.noreply.github.com>
2024-07-09 22:16:48 +0900

    ♻️ Toast Provider 렌더링 위치 변경 및 Portal Div 생성

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,9 +1,12 @@
+import { ToastProvider } from '@Components/ToastProvider';
 import { Outlet } from 'react-router-dom';
 
 export default function RootLayout() {
   return (
     <div id="rootLayout" className="relative mx-auto h-1 min-h-dvh w-full overflow-hidden border-x md:max-w-[48rem]">
       <Outlet />
+      <div id="portalSection" />
+      <ToastProvider />
     </div>
   );
 }
