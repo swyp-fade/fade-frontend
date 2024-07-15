42dbcd9657f3f13fff4eafcd17cd76d58a5f832d
sangpok <48979587+sangpok@users.noreply.github.com>
2024-07-15 21:26:25 +0900

    ✨ Unsaved Change 핸들링을 위한 setCloseHandler 선언 및 호출

--- src/stores/modal.ts
+++ src/stores/modal.ts
@@ -23,6 +23,7 @@ export type ModalType = 'fullScreenDialog' | 'bottomSheet' | 'component';
 
 /** T: 반환값 / P: Props */
 export type DefaultModalProps<T = unknown, P = Record<string, unknown>> = {
+  setCloseHandler: (handler: () => () => Promise<boolean>) => void;
   onClose: (value?: T) => void;
 } & P;
 
