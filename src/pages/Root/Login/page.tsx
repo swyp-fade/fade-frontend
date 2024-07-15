716bf8fa15bae37b0410a036d025a7e1ff7eb6d5
sangpok <48979587+sangpok@users.noreply.github.com>
2024-07-15 23:14:56 +0900

    💄 온보딩 배경 이미지 추가

--- src/pages/Root/Login/page.tsx
+++ src/pages/Root/Login/page.tsx
@@ -2,9 +2,11 @@ import { FadeLogo } from '@Components/FadeLogo';
 import { Carousel } from './components/Carousel';
 import { KakaoLoginButton } from './components/KakaoLoginButton';
 
+import onboardingBackground from '@Assets/onboarding_background.jpg';
+
 export default function Page() {
   return (
-    <section className="flex h-dvh flex-col items-center justify-center">
+    <section className="flex h-dvh flex-col items-center justify-center" style={{ backgroundImage: `url('${onboardingBackground}')`, backgroundSize: 'cover' }}>
       <div className="flex flex-1 items-center">
         <FadeLogo />
       </div>
