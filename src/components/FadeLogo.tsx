b70d612973d765fd04c7dbc0692e130dd6a4b3da
sangpok <48979587+sangpok@users.noreply.github.com>
2024-07-16 16:29:38 +0900

    🍱 페이드 로고 수정 및 반영

--- src/components/FadeLogo.tsx
+++ src/components/FadeLogo.tsx
@@ -1,5 +1,5 @@
 import fadeLogoImage from '@Assets/fade_logo.png';
 
 export function FadeLogo() {
-  return <div className="h-6 w-[5.25rem]" style={{ backgroundImage: `url('${fadeLogoImage}')` }} />;
+  return <div className="h-8 w-[7.125rem]" style={{ backgroundImage: `url('${fadeLogoImage}')` }} />;
 }
