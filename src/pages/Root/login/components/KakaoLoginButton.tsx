34debb734426fdfdb6e0c2195c1f3d3a0887b1d7
sangpok <48979587+sangpok@users.noreply.github.com>
2024-07-21 19:52:32 +0900

    📱 카카오 로그인 이미지 2dpr 대응

--- src/pages/Root/login/components/KakaoLoginButton.tsx
+++ src/pages/Root/login/components/KakaoLoginButton.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import kakaoLoginImage from '@Assets/kakao_login.png';
+import kakaoLogimImage2x from '@Assets/kakao_login@2x.png';
 
 export function KakaoLoginButton() {
   const { VITE_KAKAO_API_KEY: apiKey, VITE_KAKAO_REDIRECT_URL: redirectURL } = import.meta.env;
@@ -7,7 +8,11 @@ export function KakaoLoginButton() {
 
   return (
     <Link
-      style={{ backgroundImage: `url('${kakaoLoginImage}')`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat' }}
+      style={{
+        backgroundImage: `image-set(url('${kakaoLoginImage}') 1x, url('${kakaoLogimImage2x}') 2x)`,
+        backgroundSize: 'contain',
+        backgroundRepeat: 'no-repeat',
+      }}
       to={kakaoLoginURL}
       className="h-[2.625rem] w-[20rem] rounded transition-transform touchdevice:active:scale-95 pointerdevice:hover:scale-105 pointerdevice:active:scale-95"
     />
