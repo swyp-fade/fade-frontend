413d9d3b3ee9315e4c46dc832ee970cc63fff875
sangpok <48979587+sangpok@users.noreply.github.com>
2024-07-21 20:34:39 +0900

    📱 투표 시작 및 투표 방법 버튼 클릭 피드백 추가

--- src/pages/Root/voteFAP/components/ReadyToVoteView.tsx
+++ src/pages/Root/voteFAP/components/ReadyToVoteView.tsx
@@ -33,7 +33,9 @@ function HowToVoteButton() {
 
   return (
     <button className="group rounded-lg border-gray-200 bg-white p-2 text-xl shadow-bento" onClick={showHowToVoteModal}>
-      <span className="inline-block transition-transform pointerdevice:group-hover:scale-105 pointerdevice:group-active:scale-95">투표 방법</span>
+      <span className="inline-block transition-transform touchdevice:group-active:scale-95 pointerdevice:group-hover:scale-105 pointerdevice:group-active:scale-95">
+        투표 방법
+      </span>
     </button>
   );
 }
@@ -50,7 +52,9 @@ function ReadyToVoteTools({ onStartClick }: { onStartClick: () => void }) {
 function StartVoteButton({ onClick }: { onClick: () => void }) {
   return (
     <button className="group rounded-lg bg-purple-700 p-3 text-xl font-semibold text-white shadow-bento" onClick={onClick}>
-      <span className="inline-block transition-transform pointerdevice:group-hover:scale-105 pointerdevice:group-active:scale-95">투표 시작하기</span>
+      <span className="inline-block transition-transform touchdevice:group-active:scale-95 pointerdevice:group-hover:scale-105 pointerdevice:group-active:scale-95">
+        투표 시작하기
+      </span>
     </button>
   );
 }
