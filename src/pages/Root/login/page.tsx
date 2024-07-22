4f118594de815a75c601720d52f4d19cf498cec4
sangpok <48979587+sangpok@users.noreply.github.com>
2024-07-22 15:37:15 +0900

    💄 온보딩 레이아웃 배치 수정
    5:12:8

--- src/pages/Root/login/page.tsx
+++ src/pages/Root/login/page.tsx
@@ -1,22 +1,20 @@
 import { FadeLogo } from '@Components/FadeLogo';
+import { Link } from 'react-router-dom';
 import { Carousel } from './components/Carousel';
 import { KakaoLoginButton } from './components/KakaoLoginButton';
 
-import onboardingBackground from '@Assets/onboarding_background.jpg';
-import { Link } from 'react-router-dom';
-
 export default function Page() {
   return (
-    <section className="flex h-dvh flex-col items-center justify-center" style={{ backgroundImage: `url('${onboardingBackground}')`, backgroundSize: 'cover' }}>
-      <div className="flex flex-1 items-center">
+    <section className="flex h-dvh flex-col items-center justify-center">
+      <div className="flex flex-[0.5] items-center">
         <FadeLogo />
       </div>
 
-      <div className="flex flex-1 items-center">
+      <div className="flex w-full flex-[1.2] items-center justify-center">
         <Carousel />
       </div>
 
-      <div className="flex flex-1 items-center">
+      <div className="flex flex-[0.8] items-center">
         <div className="flex flex-col gap-3">
           <KakaoLoginButton />
           <Link to="/auth/callback/kakao?code=test" className="rounded-lg border bg-gray-50 p-2 text-center">
