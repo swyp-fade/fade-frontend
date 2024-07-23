b9657a06cb4a5cb0d7188f76b698af4dc630442b
sangpok <48979587+sangpok@users.noreply.github.com>
2024-07-24 02:01:16 +0900

    ✨ 투표 선택지 API 연동을 위한 전역 변수 추가

--- src/stores/vote.ts
+++ src/stores/vote.ts
@@ -1,21 +1,36 @@
+import { generateRandomId } from '@Utils/index';
 import { create } from 'zustand';
 
-export type FashionCard = {
-  userId: string;
+export type VoteCandidateCardType = {
+  feedId: number;
+  userId: number;
   imageURL: string;
+  anonName: string;
 };
 
 export type SwipeDirection = 'left' | 'right';
 
+export type VoteType = 'FADE_IN' | 'FADE_OUT';
+
+export type VoteResultItem = {
+  feedId: number;
+  voteType: VoteType;
+};
+
 interface VotingState {
-  viewCards: FashionCard[]; // 현재 투표 선택지
+  cycleId: string; // 현재 투표 사이클 Id
+  viewCards: VoteCandidateCardType[]; // 현재 투표 선택지
+  voteResults: VoteResultItem[]; // 투표 결과를 담아놓는 곳
   hasVotedToday: boolean; // 오늘 투표를 진행했는지?
   isVotingInProgress: boolean; // 현재 투표 중인지?
   votingCountToday: number; // 오늘 투표한 횟수
   votingProgress: number; // 1부터 시작, 1: 1을 투표할 차례라는 뜻
   swipeDirection: SwipeDirection; // 애니메이션을 위한 스와이프 방향
 
-  setViewCards: (viewCards: FashionCard[]) => void;
+  generateNewCycleId: () => void;
+  setViewCards: (viewCards: VoteCandidateCardType[]) => void;
+  updateVoteResult: (voteResult: VoteResultItem) => void;
+  clearVoteResults: () => void;
   setHasVotedToday: (hasVoted: boolean) => void;
   setIsVotingInProgress: (isInProgress: boolean) => void;
   addVotingCountToday: () => void;
@@ -27,14 +42,19 @@ interface VotingState {
 }
 
 export const useVotingStore = create<VotingState>((set, get) => ({
+  cycleId: generateRandomId(),
   viewCards: [],
+  voteResults: [],
   hasVotedToday: false,
   isVotingInProgress: false,
   votingCountToday: 0,
   votingProgress: 1,
   swipeDirection: 'right',
 
+  generateNewCycleId: () => set({ cycleId: generateRandomId() }),
   setViewCards: (viewCards) => set({ viewCards }),
+  updateVoteResult: (voteResult) => set(({ voteResults }) => ({ voteResults: [...voteResults, voteResult] })),
+  clearVoteResults: () => set({ voteResults: [] }),
   setHasVotedToday: (hasVotedToday) => set({ hasVotedToday }),
   setIsVotingInProgress: (isVotingInProgress) => set({ isVotingInProgress }),
   addVotingCountToday: () => set(({ votingCountToday }) => ({ votingCountToday: votingCountToday + 1 })),
@@ -43,7 +63,12 @@ export const useVotingStore = create<VotingState>((set, get) => ({
   setSwipeDirection: (swipeDirection) => set({ swipeDirection }),
 
   handleSelect: (swipeDirection) => {
-    const { viewCards, setViewCards, setSwipeDirection, addVotingCountToday, addVotingProgress } = get();
+    const { viewCards, setViewCards, setSwipeDirection, addVotingCountToday, addVotingProgress, updateVoteResult } = get();
+    const currentViewCard = viewCards.at(-1);
+
+    if (currentViewCard) {
+      updateVoteResult({ feedId: currentViewCard.feedId, voteType: swipeDirection === 'left' ? 'FADE_OUT' : 'FADE_IN' });
+    }
 
     setViewCards(viewCards.slice(0, -1));
     setSwipeDirection(swipeDirection);
