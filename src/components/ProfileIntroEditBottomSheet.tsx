cd2f7b8418bfdb7de60e8f545fe4688c82aaeed7
sangpok <48979587+sangpok@users.noreply.github.com>
2024-07-26 17:32:03 +0900

    ✨ 프로필 소개 편집 Bottom Sheet 작성

---
+++ src/components/ProfileIntroEditBottomSheet.tsx
@@ -0,0 +1,47 @@
+import { FlexibleLayout } from '@Layouts/FlexibleLayout';
+import { DefaultModalProps } from '@Stores/modal';
+import { forwardRef, useState } from 'react';
+import { MdClose } from 'react-icons/md';
+
+type ProfileIntroEditBottomSheetProps = { defaultProfileIntro: string };
+
+export const ProfileIntroEditBottomSheet = forwardRef<HTMLDivElement, DefaultModalProps<void, ProfileIntroEditBottomSheetProps>>(
+  ({ defaultProfileIntro, onClose }: DefaultModalProps<void, ProfileIntroEditBottomSheetProps>, ref) => {
+    const [profileIntro, setProfileIntro] = useState(defaultProfileIntro);
+    const textLength = profileIntro.length;
+
+    return (
+      <FlexibleLayout.Root ref={ref} className="h-fit">
+        <FlexibleLayout.Header>
+          <header className="relative flex flex-row items-center justify-between px-5 pt-4">
+            <p className="text-xl font-semibold">프로필 소개 편집</p>
+            <button type="button" onClick={() => onClose()} className="group rounded-lg p-1 touchdevice:active:bg-gray-100 pointerdevice:hover:bg-gray-100">
+              <MdClose className="size-6 text-gray-600 transition-transform group-active:scale-95" />
+            </button>
+          </header>
+        </FlexibleLayout.Header>
+
+        <FlexibleLayout.Content className="p-5">
+          <div className="flex h-[10.25rem] w-full resize-none flex-col rounded-lg bg-gray-100 p-3">
+            <textarea
+              className="h-full w-full resize-none bg-transparent align-text-top outline-none transition-colors disabled:bg-gray-300 disabled:text-gray-500"
+              placeholder="멋지게 나를 소개해 보아요!"
+              value={profileIntro}
+              onChange={(e) => setProfileIntro(e.target.value)}
+              maxLength={150}
+            />
+            <p className="text-right text-xs text-gray-400">{textLength > 150 ? 150 : textLength} / 200</p>
+          </div>
+        </FlexibleLayout.Content>
+
+        <FlexibleLayout.Footer>
+          <div className="flex p-4">
+            <button type="button" className="group flex-1 rounded-lg bg-purple-500 py-2 text-xl text-white transition-colors" onClick={() => onClose()}>
+              <span className="inline-block transition-transform group-active:scale-95">완료</span>
+            </button>
+          </div>
+        </FlexibleLayout.Footer>
+      </FlexibleLayout.Root>
+    );
+  }
+);
