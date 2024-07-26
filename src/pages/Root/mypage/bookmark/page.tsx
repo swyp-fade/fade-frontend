3de47255dafa840d6cdc68c8bf08a7d7eba663c4
sangpok <48979587+sangpok@users.noreply.github.com>
2024-07-26 18:49:26 +0900

    ✨ 북마크 페이지 화면 구현

---
+++ src/pages/Root/mypage/bookmark/page.tsx
@@ -0,0 +1,40 @@
+import { useHeader } from '@Hooks/useHeader';
+import { MdChevronLeft } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
+import testImage from '@Assets/test_fashion_image.jpg';
+
+export default function Page() {
+  useHeader({
+    title: '북마크',
+    leftSlot: () => <BackButton />,
+  });
+
+  return (
+    <div>
+      <div className="grid w-full grid-cols-3 gap-1 p-1">
+        {Array.from({ length: 13 })
+          .fill(0)
+          .map((_, index) => (
+            <div key={`item-${index}`} className="group aspect-[3/4] w-full cursor-pointer overflow-hidden rounded-lg">
+              <div
+                style={{ backgroundImage: `url('${testImage}')` }}
+                className="h-full w-full bg-cover bg-center bg-no-repeat transition-transform group-hover:scale-105"
+              />
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+}
+
+function BackButton() {
+  const navigate = useNavigate();
+
+  return (
+    <button
+      className="group cursor-pointer rounded-lg p-2 touchdevice:active:bg-gray-100 pointerdevice:hover:bg-gray-100"
+      onClick={() => navigate('/mypage', { replace: true })}>
+      <MdChevronLeft className="size-6 transition-transform group-active:scale-95" />
+    </button>
+  );
+}
