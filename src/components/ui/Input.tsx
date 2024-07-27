dee61934c4a57c8447f004aaafc2d44141da5be8
sangpok <48979587+sangpok@users.noreply.github.com>
2024-07-27 17:18:00 +0900

    ♻️ Input props 변경

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -4,8 +4,8 @@ import { MdHighlightOff, MdOutlineCheckCircle } from 'react-icons/md';
 
 export const Input = forwardRef<HTMLDivElement, InputHTMLAttributes<HTMLInputElement>>(({ className, ...props }: InputHTMLAttributes<HTMLInputElement>, ref) => {
   return (
-    <div ref={ref} className={cn(className, 'peer flex flex-row rounded-lg border border-gray-200 px-5 py-[.875rem] focus-within:border-purple-500')} {...props}>
-      <input className={'w-full appearance-none outline-none'} />
+    <div ref={ref} className={cn(className, 'peer flex flex-row rounded-lg border border-gray-200 px-5 py-[.875rem] focus-within:border-purple-500')}>
+      <input className={'w-full appearance-none outline-none'} {...props} />
       {props['aria-invalid'] === false && props.value !== '' && <MdOutlineCheckCircle className="size-6 text-purple-500" />}
       {props['aria-invalid'] === true && <MdHighlightOff className="size-6 text-pink-600" />}
     </div>
