5b8d89a2ebe7cd20a30825bacaf035d5149453df
dxdlar <dxdlar@gmail.com>
2024-07-27 12:49:34 +0900

    ♻️ Tooltip 컴포넌트 분리

--- src/components/LockUI.tsx
+++ src/components/LockUI.tsx
@@ -10,10 +10,6 @@ export default function LockUI() {
         <p className="text-center text-h2 font-medium">오늘의 투표를 완료해주세요!</p>
         <p className="px-2 text-center text-h6 font-light leading-tight text-gray-900">하루 최소 1사이클(10회) 이상 FA:P 투표를 완료해야 다른 기능을 이용할 수 있어요.</p>
       </div>
-      <div className="absolute bottom-2 left-[20%]">
-        <div className="relative break-words rounded-2xl bg-purple-500 px-6 py-2 text-center text-white">FA:P 투표하러 가기!</div>
-        <div className="mx-auto h-0 w-0 border-l-8 border-r-8 border-t-8 border-l-transparent border-r-transparent border-t-purple-500"></div>
-      </div>
     </div>
   );
 }
---
+++ src/components/Tooltip.tsx
@@ -0,0 +1,8 @@
+export default function Tooltip() {
+  return (
+    <div className="absolute bottom-2">
+      <div className="relative break-words rounded-2xl bg-purple-500 px-6 py-2 text-center text-white">FA:P 투표하러 가기!</div>
+      <div className="mx-auto h-0 w-0 border-l-8 border-r-8 border-t-8 border-l-transparent border-r-transparent border-t-purple-500"></div>
+    </div>
+  );
+}
