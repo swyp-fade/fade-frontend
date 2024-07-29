9c45e9b055d8f9270f2a94357e5aa6072f6e8bf1
sangpok <48979587+sangpok@users.noreply.github.com>
2024-07-29 22:46:05 +0900

    ♻️ 로그인/로그아웃 이후 Authorization Header 설정 함수 호출

--- src/pages/Auth/SignOut.tsx
+++ src/pages/Auth/SignOut.tsx
@@ -1,4 +1,5 @@
 import { useAuthActions } from '@Hooks/auth';
+import { clearAuthorizationHeader } from '@Libs/axios';
 import { requestSignOut } from '@Services/auth';
 import { useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
@@ -12,6 +13,7 @@ export default function SignOut() {
 
   useEffect(() => {
     signOut();
+    clearAuthorizationHeader();
   }, []);
 
   return <Navigate to="/" replace />;
