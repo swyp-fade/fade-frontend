07c9fdd7725d24fc16c7f78c7b422c37fe9a26ca
김 주현 <48979587+sangpok@users.noreply.github.com>
2024-07-29 16:54:53 +0900

    ✨ API Mocking 버튼 추가 (#164)
    * ➕ Radix UI Popover UI 추가
    
    * ✨ API 모킹 버튼 작성
    
    * ✏️ 테스트 로그인 안내 문구 추가
    
    * 🩹 모달 노출 시 모킹 버튼 미동작 수정

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,10 +1,11 @@
+import { AsyncErrorBoundary } from '@Components/AsyncErrorBoundary';
+import { MockingButton } from '@Components/MockingButton';
 import { ModalProvider } from '@Components/ModalProvider';
 import { ToastProvider } from '@Components/ToastProvider';
 import { cn } from '@Utils/index';
 import { Outlet, useLocation } from 'react-router-dom';
 
 import onboardingBackground from '@Assets/onboarding_background.jpg';
-import { AsyncErrorBoundary } from '@Components/AsyncErrorBoundary';
 
 export default function RootLayout() {
   const { pathname } = useLocation();
@@ -24,6 +25,10 @@ export default function RootLayout() {
 
       <ModalProvider />
       <ToastProvider />
+
+      <div className="absolute left-1/2 top-4 z-50 -translate-x-1/2 opacity-30 transition-opacity focus-within:opacity-100 touchdevice:active:opacity-100 pointerdevice:hover:opacity-100">
+        <MockingButton />
+      </div>
     </div>
   );
 }
