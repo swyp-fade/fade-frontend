ad9f49edc006953bea8b2e12945c946459e16f93
dxdlar <dxdlar@gmail.com>
2024-08-01 18:22:14 +0900

    ♻️ 투표 미진행 및 투표탭이 아니면 LockUI를 표출하도록 수정

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,10 +1,16 @@
 import { Header } from '@Components/Header';
 import { NavBar } from '@Components/NavBar';
 import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { FlexibleLayout } from './FlexibleLayout';
+import { useVotingStore } from '@Stores/vote';
+import LockUI from '@Components/LockUI';
 
 export default function AppLayout() {
+  const hasVotedToday = useVotingStore((state) => state.hasVotedToday);
+  const location = useLocation();
+  const isVoteFAPPath = location.pathname === '/vote-fap';
+
   return (
     <FlexibleLayout.Root className="w-full flex-1 border border-red-500">
       <FlexibleLayout.Header>
@@ -12,13 +18,11 @@ export default function AppLayout() {
       </FlexibleLayout.Header>
 
       <FlexibleLayout.Content>
-        <Suspense fallback={<>페이지 로딩 중!</>}>
-          <Outlet />
-        </Suspense>
+        <Suspense fallback={<>페이지 로딩 중!</>}>{isVoteFAPPath || hasVotedToday ? <Outlet /> : <LockUI />}</Suspense>
       </FlexibleLayout.Content>
 
       <FlexibleLayout.Footer>
-        <NavBar />
+        <NavBar hasVotedToday={hasVotedToday} isVoteFAPPath={isVoteFAPPath} />
       </FlexibleLayout.Footer>
     </FlexibleLayout.Root>
   );
