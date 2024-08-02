aab8a2578d6ff7c82eef76a370f47970b60b6397
sangpok <y_y5760y_y@naver.com>
2024-08-02 16:48:29 +0900

    💄 마이 페이지 > 투표 내역 Skeleton UI 작성

---
+++ src/pages/Root/mypage/voteHistory/page.skeleton.tsx
@@ -0,0 +1,47 @@
+import { Grid } from '@Components/ui/grid';
+import { Skeleton } from '@Components/ui/skeleton';
+
+export default function Page() {
+  return (
+    <div className="flex h-full flex-col">
+      <div className="flex flex-row items-center gap-3 bg-white px-5 py-3">
+        <Skeleton className="h-[2.125rem] flex-1" />
+        <Skeleton className="h-full w-[8rem] rounded-full" />
+      </div>
+
+      <div id="feedList" className="flex-1 space-y-[3.75rem] overflow-y-scroll p-5">
+        <section className="space-y-2">
+          <Skeleton className="h-5 w-[16rem]" />
+          <Grid id="feedList" cols={5}>
+            <Skeleton className="aspect-[3/4] flex-1" />
+            <Skeleton className="aspect-[3/4] flex-1" />
+            <Skeleton className="aspect-[3/4] flex-1" />
+            <Skeleton className="aspect-[3/4] flex-1" />
+            <Skeleton className="aspect-[3/4] flex-1" />
+            <Skeleton className="aspect-[3/4] flex-1" />
+            <Skeleton className="aspect-[3/4] flex-1" />
+            <Skeleton className="aspect-[3/4] flex-1" />
+            <Skeleton className="aspect-[3/4] flex-1" />
+            <Skeleton className="aspect-[3/4] flex-1" />
+          </Grid>
+        </section>
+
+        <section className="space-y-2">
+          <Skeleton className="h-5 w-[16rem]" />
+          <Grid id="feedList" cols={5}>
+            <Skeleton className="aspect-[3/4] flex-1" />
+            <Skeleton className="aspect-[3/4] flex-1" />
+            <Skeleton className="aspect-[3/4] flex-1" />
+            <Skeleton className="aspect-[3/4] flex-1" />
+            <Skeleton className="aspect-[3/4] flex-1" />
+            <Skeleton className="aspect-[3/4] flex-1" />
+            <Skeleton className="aspect-[3/4] flex-1" />
+            <Skeleton className="aspect-[3/4] flex-1" />
+            <Skeleton className="aspect-[3/4] flex-1" />
+            <Skeleton className="aspect-[3/4] flex-1" />
+          </Grid>
+        </section>
+      </div>
+    </div>
+  );
+}
