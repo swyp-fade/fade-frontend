5f87160ac18fac0dcdb16e72924f58d09a5aee68
sangpok <48979587+sangpok@users.noreply.github.com>
2024-08-03 18:46:56 +0900

    ♻️ 공통 UI Component인 Image 컴포넌트 구조 변경
    div 대신 img 태그로 변경

--- src/components/ui/image.tsx
+++ src/components/ui/image.tsx
@@ -1,40 +1,46 @@
-import { cn, prefetchImages } from '@Utils/index';
-import { PropsWithChildren, useEffect, useState } from 'react';
+import { cn, createSrcSet } from '@Utils/index';
+import { PropsWithChildren, useState } from 'react';
 import { MdOutlineImageNotSupported } from 'react-icons/md';
 
 interface ImageProps {
   src: string;
   className?: string;
   alt?: string;
-  size?: 'cover' | 'contain';
+  size?: 'cover' | 'contain' | 'fit';
+  local?: boolean;
 }
 
-export function Image({ children, src, className, alt, size = 'cover' }: PropsWithChildren<ImageProps>) {
-  const [imageURL, setImageURL] = useState('');
+export function Image({ children, src, className, alt, size = 'cover', local = false }: PropsWithChildren<ImageProps>) {
+  const [isPending, setIsPending] = useState(false);
   const [isError, setIsError] = useState(false);
 
-  const hasNoImageURL = imageURL === '';
-
-  useEffect(() => {
-    prefetchImages([src])
-      .then(() => setImageURL(src))
-      .catch(() => setIsError(true));
-  }, [src]);
-
   return (
-    <div
-      role="img"
-      aria-label={alt}
-      style={{ backgroundImage: imageURL ? `url('${imageURL}')` : 'none' }}
-      className={cn('relative grid h-full w-full place-content-center bg-center bg-no-repeat', className, {
-        ['bg-cover']: size === 'cover',
-        ['bg-contain']: size === 'contain',
-      })}>
-      {hasNoImageURL && <div className="absolute inset-0 animate-pulse bg-gray-200" />}
+    <div className={cn('relative grid h-full w-full place-content-center', className)}>
+      <img
+        src={src}
+        srcSet={local ? createSrcSet(src) : src}
+        alt={alt}
+        className={cn('pointer-events-none absolute inset-0 block h-full w-full', {
+          ['[display:none]']: isError,
+          ['object-cover']: size === 'cover',
+          ['object-contain']: size === 'contain',
+          ['object-none']: size === 'fit',
+        })}
+        onLoadStart={() => setIsPending(true)}
+        onLoad={() => setIsPending(false)}
+        onError={() => setIsError(true)}
+        // loading="lazy"
+      />
+      {isPending && <div className="absolute inset-0 animate-pulse bg-gray-200" />}
       {isError && <MdOutlineImageNotSupported className="size-4 text-pink-400" />}
+      <PreventImageDragging />
       {children}
     </div>
   );
+}
+
+function PreventImageDragging() {
+  return <div className="absolute h-full w-full" />;
 }
 
 /** 아래의 코드는 Lazy와 Caching을 이용한 Image 컴포넌트인데... 이상하게 너무 버벅여서 일단 내버려뒀음. 아마 Blob을 생성하는 과정에서 무언가 있는 것 같은데 ..... 최적화 때 둘러보는 걸로. */
