366b8a71b5b51631f99e60b419ae5fc46c842920
김 주현 <48979587+sangpok@users.noreply.github.com>
2024-08-06 02:53:56 +0900

    💄 서비스 잠금 화면 Lock UI 수정 (#244)
    * 🩹 서비스 잠금 화면 노출 시 헤더 조정
    
    * 💄 툴팁 UI 조정

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,7 +1,7 @@
 export default function Tooltip() {
   return (
-    <div className="absolute bottom-16">
-      <div className="relative break-words rounded-2xl bg-purple-500 px-6 py-2 text-center text-white">FA:P 투표하러 가기!</div>
+    <div className="absolute bottom-full left-1/2 mb-3 -translate-x-1/2">
+      <div className="relative whitespace-nowrap rounded-2xl bg-purple-500 px-6 py-2 text-center text-white">FA:P 투표하러 가기!</div>
       <div className="mx-auto h-0 w-0 border-l-8 border-r-8 border-t-8 border-l-transparent border-r-transparent border-t-purple-500"></div>
     </div>
   );
