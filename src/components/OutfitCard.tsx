0878fc953c4cb72c969875c2fe624f40cae759e2
sangpok <48979587+sangpok@users.noreply.github.com>
2024-08-07 18:30:16 +0900

    🩹 OutfitCard에서 wouldShowDetail 속성 제거

--- src/components/OutfitCard.tsx
+++ src/components/OutfitCard.tsx
@@ -2,20 +2,18 @@ import { OUTFIT_CATEGORY_LIST } from '@/constants';
 import { TOutfitItem } from '@Types/model';
 import { ItemBadge } from './ItemBadge';
 
-interface TOutfitCard {
-  wouldShowDetail?: boolean;
-}
+interface TOutfitCard {}
 
 type OutfitCardProps = TOutfitCard & Omit<TOutfitItem, 'id'>;
 
-export function OutfitCard({ brandName, categoryId, details, wouldShowDetail = true }: OutfitCardProps) {
+export function OutfitCard({ brandName, categoryId, details }: OutfitCardProps) {
   return (
     <div className="flex w-full flex-row gap-3 rounded-lg border border-purple-50 bg-white p-3">
       <ItemBadge variants="primary">{OUTFIT_CATEGORY_LIST.at(categoryId)}</ItemBadge>
 
-      <div className="flex w-full flex-col justify-center gap-1">
+      <div className="flex w-full flex-col justify-center">
         <p className="text-left">{brandName}</p>
-        {wouldShowDetail && <p className="text-left text-sm text-gray-500">{details}</p>}
+        {details && <p className="text-left text-sm text-gray-500">{details}</p>}
       </div>
     </div>
   );
