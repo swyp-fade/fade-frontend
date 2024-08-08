41f039b8b103217a48f877c07ff6670425d29c35
sangpok <48979587+sangpok@users.noreply.github.com>
2024-08-08 17:30:25 +0900

    🩹 기존 모킹 버튼 기능 변경

--- src/components/MockingButton.tsx
+++ src/components/MockingButton.tsx
@@ -1,66 +1,50 @@
 import * as Popover from '@radix-ui/react-popover';
 import { useVotingStore } from '@Stores/vote';
-import { cn } from '@Utils/index';
-import { useRef, useState } from 'react';
 import { MdSettings } from 'react-icons/md';
 import { Button } from './ui/button';
-
-const LOCALSTORAGE_KEY = 'FADE_API_MOCKING_ENABLED' as const;
+import { useEffect, useState } from 'react';
 
 export function MockingButton() {
-  const mockButtonRef = useRef<HTMLButtonElement>(null);
   const setHasVotedToday = useVotingStore((state) => state.setHasVotedToday);
+  const [isMockingEnabled, setIsMockingEnabled] = useState(false);
 
-  const [isMockEnabled, setIsMockEnabled] = useState(() => {
-    return (localStorage.getItem(LOCALSTORAGE_KEY) as 'true' | 'false' | undefined) === 'true' ? true : false;
-  });
+  useEffect(() => {
+    const handleMockingStart = () => setIsMockingEnabled(true);
+    const handleMockingEnd = async () => {
+      setIsMockingEnabled(false);
 
-  const handleClick = async () => {
-    if (mockButtonRef.current === null) {
-      return;
-    }
+      // MSW 중지
+      const { worker } = await import('@/__mock__/instance');
+      worker.stop();
 
-    const { worker } = await import('../__mock__/instance');
+      location.reload();
+    };
 
-    isMockEnabled && worker.stop();
-    !isMockEnabled && worker.start({ onUnhandledRequest: 'bypass' });
+    window.addEventListener('mockingStart', handleMockingStart);
+    window.addEventListener('mockingEnd', handleMockingEnd);
 
-    setIsMockEnabled((prev) => !prev);
-    localStorage.setItem(LOCALSTORAGE_KEY, String(!isMockEnabled));
-  };
+    return () => {
+      window.removeEventListener('mockingStart', handleMockingStart);
+      window.removeEventListener('mockingEnd', handleMockingEnd);
+    };
+  }, []);
+
+  if (!isMockingEnabled) {
+    return null;
+  }
 
   return (
     <Popover.Root>
       <Popover.Trigger asChild>
-        <Button
-          variants="outline"
-          interactive="onlyScale"
-          size="icon"
-          className={cn('pointer-events-auto border border-gray-200 bg-white shadow-2xl', {
-            ['border-red-400 bg-red-300']: isMockEnabled,
-          })}>
+        <Button variants="outline" interactive="onlyScale" size="icon" className="pointer-events-auto border border-red-400 bg-red-300 shadow-2xl">
           <MdSettings />
         </Button>
       </Popover.Trigger>
       <Popover.Portal>
         <Popover.Content className="translate-y-2 rounded-xl border border-gray-200 bg-white p-5 shadow-2xl">
           <Popover.Arrow className="fill-white stroke-gray-200" />
+          <p className="mb-3 whitespace-pre-line text-center text-detail">{`서비스 둘러보기 시에만\n나타나는 테스트 버튼입니다`}</p>
           <ul className="flex flex-col">
-            <li className="w-full">
-              <Button
-                ref={mockButtonRef}
-                aria-checked={(localStorage.getItem('FADE_API_MOCKING_ENABLED') as 'true' | 'false' | undefined) || 'false'}
-                variants="ghost"
-                className={cn('w-full', { ['text-pink-400']: isMockEnabled })}
-                onClick={handleClick}>
-                {isMockEnabled ? 'Disable' : 'Enable'} API Mocking
-              </Button>
-            </li>
-            <li className="w-full">
-              <Button variants="ghost" className="w-full" onClick={() => location.reload()}>
-                Page Reload
-              </Button>
-            </li>
             <li className="w-full">
               <Button variants="ghost" className="w-full" onClick={() => setHasVotedToday(true)}>
                 Bypass the vote check today
