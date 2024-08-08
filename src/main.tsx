4638dfa953166bbbc61c37ccd9517ba2b35675f6
sangpok <48979587+sangpok@users.noreply.github.com>
2024-08-08 17:29:23 +0900

    🔥 API 모킹 동작 변경으로 인한 React DOM Render 로직 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,5 @@
 import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
 import './index.css';
 
-export async function enableMocking(enabled: boolean = false) {
-  // if (import.meta.env.PROD || !enabled) {
-  if (!enabled) {
-    return;
-  }
-
-  const { worker } = await import('./__mock__/instance.ts');
-
-  return worker.start({
-    onUnhandledRequest: 'bypass',
-  });
-}
-
-const LOCALSTORAGE_KEY = 'FADE_API_MOCKING_ENABLED' as const;
-
-const isMockEnabled = (localStorage.getItem(LOCALSTORAGE_KEY) as 'true' | 'false' | undefined) === 'true' ? true : false;
-
-enableMocking(isMockEnabled).then(() => {
-  import('./App.tsx').then((App) => {
-    ReactDOM.createRoot(document.getElementById('root')!).render(<App.default />);
-  });
-});
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
