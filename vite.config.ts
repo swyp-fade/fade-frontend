89f0383681af2328bb1c290dba061aff00e16b36
sangpok <y_y5760y_y@naver.com>
2024-08-27 20:08:51 +0900

    👷 분류에 따른 빌드 청크 분리
    @lottie-vendor, @lib-vendor, @react-vendor 분류

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import mkcert from 'vite-plugin-mkcert';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()],
+  plugins: [react(), mkcert(), visualizer()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.includes('lottie')) {
+            return '@lottie-vendor';
+          }
+          if (id.includes('axios') || id.includes('date-fns') || id.includes('framer-motion')) {
+            return '@lib-vendor';
+          }
+          if (id.includes('node_modules/react/') || id.includes('node_modules/react-dom/')) {
+            return '@react-vendor';
+          }
+        },
+      },
+    },
+  },
   resolve: {
     alias: [
       { find: '@Components', replacement: '/src/components' },
