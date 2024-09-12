3d71a30fa75e5d7470735387a2e42a246b051d9f
sangpok <y_y5760y_y@naver.com>
2024-09-12 16:49:25 +0900

    ♻️ 헤더 컴포넌트  타이틀 위치 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,14 @@ export function Header() {
   const rightSlot = useHeaderStore((state) => state.rightSlot);
   const title = useHeaderStore((state) => state.title);
 
+  const isTitleString = typeof title === 'string';
+  const isTitleSlot = typeof title === 'function';
+
   return (
     <header className="relative py-2">
       {leftSlot && <div className="absolute left-3 top-1/2 -translate-y-1/2">{leftSlot()}</div>}
-      {title && <p className="text-center text-2xl font-semibold">{title}</p>}
+      {isTitleString && <p className="text-center text-2xl font-semibold">{title}</p>}
+      {isTitleSlot && <div className="mx-auto w-fit">{title()}</div>}
       {rightSlot && <div className="absolute right-3 top-1/2 -translate-y-1/2">{rightSlot()}</div>}
     </header>
   );
