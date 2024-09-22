05c9eb3ada2b0088d7687710d1d6144a732eb646
김 주현 <48979587+sangpok@users.noreply.github.com>
2024-09-22 20:43:10 +0900

    ⚡️ 이미지 크롭 퍼포먼스 최적화 (#334)
    ⚡️ 이미지 크롭 리렌더링으로 인한 Throttling 해결

--- src/components/forms/bon/_components/ImageCropperModal.tsx
+++ src/components/forms/bon/_components/ImageCropperModal.tsx
@@ -1,68 +1,80 @@
 import { Button } from '@Components/ui/button';
+import { useToastActions } from '@Hooks/toast';
 import { DefaultModalProps } from '@Stores/modal';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Cropper, ReactCropperElement } from 'react-cropper';
 import { MdChevronLeft } from 'react-icons/md';
+import { VscLoading } from 'react-icons/vsc';
 import './cropper.css';
 
 type ImageCropperModalProps = DefaultModalProps<unknown, { imageFile: File }>;
 
 export function ImageCropperModal({ imageFile, onClose }: ImageCropperModalProps) {
-  const cropperRef = useRef<ReactCropperElement>(null);
-  const croppedDataBlob = useRef<Blob>(new Blob());
+  const { showToast } = useToastActions();
+  const [doesConverting, setDoesConverting] = useState(false);
 
+  const cropperRef = useRef<ReactCropperElement>(null);
   const fileImageURL = URL.createObjectURL(imageFile);
 
   useEffect(() => {
-    return () => {
-      URL.revokeObjectURL(fileImageURL);
-    };
+    return () => URL.revokeObjectURL(fileImageURL);
   });
 
-  function dataURLtoBlob(dataURL: string) {
-    const arr = dataURL.split(',');
-    const matches = arr[0].match(/:(.*?);/);
-    const mime = matches && matches.length >= 2 ? matches[1] : null;
+  async function dataURLtoBlob(dataURL: string) {
+    return new Promise<Blob>((resolve, reject) => {
+      const arr = dataURL.split(',');
+      const matches = arr[0].match(/:(.*?);/);
+      const mime = matches && matches.length >= 2 ? matches[1] : null;
 
-    // MIME 타입이 없을 때 크롭 리셋
-    if (!mime) {
-      const cropperInstance = cropperRef.current?.cropper;
-      cropperInstance?.reset();
-      return;
-    }
+      // MIME 타입이 없을 때 크롭 리셋
+      if (!mime) {
+        const cropperInstance = cropperRef.current!.cropper;
+        cropperInstance.reset();
+        reject();
+        return;
+      }
 
-    const bstr = atob(arr[1]);
-    let n = bstr.length;
-    const u8arr = new Uint8Array(n);
+      const bstr = atob(arr[1]);
+      let n = bstr.length;
+      const u8arr = new Uint8Array(n);
 
-    while (n--) {
-      u8arr[n] = bstr.charCodeAt(n);
-    }
-    return new Blob([u8arr], { type: mime });
+      while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
+      }
+
+      resolve(new Blob([u8arr], { type: mime }));
+    });
   }
 
-  const handleCrop = () => {
-    const cropper = cropperRef?.current?.cropper;
-    const croppedDataURL = cropper?.getCroppedCanvas().toDataURL();
+  const handleApplyClick = () => {
+    const cropper = cropperRef.current!.cropper;
+    const croppedDataURL = cropper.getCroppedCanvas().toDataURL();
 
-    if (croppedDataBlob.current !== undefined) {
-      croppedDataBlob.current = dataURLtoBlob(croppedDataURL!)!;
-    }
+    setDoesConverting(true);
+
+    dataURLtoBlob(croppedDataURL)
+      .then((convertedBlob) => onClose(convertedBlob))
+      .catch(() => {
+        showToast({
+          type: 'error',
+          title: '이미지 크기 조절 오류',
+          description: '다시 시도해주세요.',
+        });
+
+        setDoesConverting(false);
+      });
   };
 
   return (
     <div className="relative flex flex-1 flex-col">
       <div className="flex flex-row justify-between p-2">
-        <Button variants="ghost" size="icon" onClick={() => onClose()}>
+        <Button variants="ghost" size="icon" onClick={() => onClose()} disabled={doesConverting}>
           <MdChevronLeft className="size-6" />
         </Button>
 
-        <Button
-          variants="ghost"
-          onClick={() => {
-            onClose(croppedDataBlob.current);
-          }}>
-          확인
+        <Button variants="ghost" onClick={handleApplyClick} disabled={doesConverting}>
+          {!doesConverting && '확인'}
+          {doesConverting && <VscLoading className="animate-spin" />}
         </Button>
       </div>
 
@@ -79,7 +91,6 @@ export function ImageCropperModal({ imageFile, onClose }: ImageCropperModalProps
         autoCropArea={1}
         checkOrientation={false}
         guides={true}
-        crop={handleCrop}
         ref={cropperRef}
       />
     </div>
